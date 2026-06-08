3b419683a643005beece97322bc84463bbd5b456
ZackyKage <elijah.rivera.studies@gmail.com>
2026-06-08 13:58:36 +0800

    Login
    Login

--- gp-system/app/Config/Routes.php
+++ gp-system/app/Config/Routes.php
@@ -4,3 +4,6 @@ use CodeIgniter\Router\RouteCollection;
 
 /** @var RouteCollection $routes */
 $routes->get('/', 'Home::index');
+
+$routes->add('/login', 'Auth::index');
+$routes->add('/dashboard', 'Pages\Dashboard::index');
