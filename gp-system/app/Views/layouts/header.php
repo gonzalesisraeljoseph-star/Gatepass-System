cd99ce5d93dc0f6bdc021249a0e36aaa0901694b
ZackyKage <elijah.rivera.studies@gmail.com>
2026-06-10 12:47:54 +0800

    Update header.php

--- gp-system/app/Views/layouts/header.php
+++ gp-system/app/Views/layouts/header.php
@@ -9,26 +9,26 @@
 <body>
     <nav id="navbar-header" class="navbar navbar-expand-lg ">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-      <div class="theme-toggle">
+              <a class="navbar-brand" href="#">Navbar</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="#">Home</a>
+                 </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Features</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Pricing</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                </li>
+            </ul>
+          <div class="theme-toggle">
             <button class="theme-btn" id="btn-snow" onclick="setTheme('snow')" aria-label="Light mode">
                 <i class="fa-solid fa-sun" aria-hidden="true"></i>
                 Light
@@ -37,9 +37,9 @@
                 <i class="fa-solid fa-moon" aria-hidden="true"></i>
                 Dark
             </button>
-    </div>
-    </div>
+          </div>
+        </div>
     
-  </div>
+      </div>
    
 </nav>
